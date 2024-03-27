ec07086e077c159f0457ada038c6105b3adb5cb9
Colby Gatty <colbysocials@gmail.com>
2024-03-27 11:23:07 -0700

    Added Slug for blog page posts, added additional responsiveness

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,6 +1,30 @@
+import Image from "next/image"
+import styles from "./singlePost.module.css"
+
 const SinglePostPage = () => {
     return (
-        <div>Single Post Page</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image className={styles.img} src="https://images.pexels.com/photos/20253067/pexels-photo-20253067/free-photo-of-roshkodol.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill/>
+            </div>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Title</h1>
+                <div className={styles.detail}>
+                <Image className={styles.userImage}src="https://images.pexels.com/photos/20253067/pexels-photo-20253067/free-photo-of-roshkodol.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width={50} height={50}/>
+                <div className={styles.detailText}>
+                    <span className={styles.detailTitle}>Author</span>
+                    <span className={styles.detailValue}>Colby Gatty</span>
+                </div>
+                <div className={styles.detailText}>
+                    <span className={styles.detailTitle}>Publised</span>
+                    <span className={styles.detailValue}>03.26.2024</span>
+                </div>
+                </div>
+                <div className={styles.content}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quae, quos et impedit nulla corporis dolorem debitis qui aliquam, corrupti aspernatur iure ab vero, odit eos error quisquam ut itaque?
+                </div>
+            </div>
+        </div>
     )
 }
 
