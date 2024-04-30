ec8fe645bdea4480e7ca10acb2dea8a3426fbd5a
Colby Gatty <colbysocials@gmail.com>
2024-04-30 16:52:21 -0700

    Calls data when blog page visit

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,39 +1,44 @@
+import { useEffect, useState } from 'react';
 import PostCard from '@/components/postCard/postCard';
 import styles from './blog.module.css';
-import { getPosts } from '@/lib/data';
 
-//FETCH DATA FROM API
-const getData = async () => {
-  const res = await fetch("https://fitness-archives.vercel.app/api/blog", {cache: 'no-store'})
+// FETCH DATA FROM API
+const fetchData = async () => {
+  const res = await fetch("https://fitness-archives.vercel.app/api/blog", { cache: 'no-store' });
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
 
-  return res.json()
+  return res.json();
+};
 
-}
+const BlogPage = () => {
+  const [posts, setPosts] = useState([]);
 
-const BlogPage = async () => {
-  
-  //FETCH DATA FROM API
-  const posts = await getData()
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const fetchedPosts = await fetchData();
+        setPosts(fetchedPosts.reverse()); // Reverse the posts if needed right when data is fetched
+      } catch (error) {
+        console.error('Failed to fetch posts:', error);
+        // Handle errors, e.g., by setting state to show an error message
+      }
+    };
 
-  // Reverse the order of posts
-  const reversedPosts = posts.reverse();
+    getData();
+  }, []); // The empty array ensures this effect runs only once when the component mounts
 
   return (
-    <div>
-  
     <div className={styles.container}>
-      {reversedPosts.map((post) => (
+      {posts.map((post) => (
         <div className={styles.post} key={post.id}>
-      <a><PostCard post={post}/></a>
-      </div>
-  ))}
-  </div>
-  </div>
+          <PostCard post={post} />
+        </div>
+      ))}
+    </div>
   );
-}
+};
 
-export default BlogPage
+export default BlogPage;
