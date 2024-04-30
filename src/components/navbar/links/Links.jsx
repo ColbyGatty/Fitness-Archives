c3b35d1ff16db2ce5099a41de8fb14aa88525c81
Colby Gatty <colbysocials@gmail.com>
2024-04-30 13:15:51 -0700

    Fixed mobile responivness

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import styles from "./links.module.css"
 import NavLink from "./navLink/navLink";
 import Image from "next/image";
@@ -30,39 +30,53 @@ const links = [
 
 const Links = ({session}) => {
     const [open,setOpen] = useState(false)
-
+    useEffect(() => {
+        if (open) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    }, [open]);
 
     //TEMPORARY
-    const isAdmin = true
+    // const isAdmin = true
 
     return (
-    <div className={styles.container}>
-        <div className={styles.links}>
-            {links.map((link=>(
-                <NavLink item={link} key={link.title}/>
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map((link=>(
+                    <NavLink item={link} key={link.title}/>
                 )))}
-                    {session?.user ? (
-                        <>
-                    {session.user?.isAdmin && (<NavLink item={{title: "Admin", path: "/admin"}}/>)}
-                    <form action={handleLogout}>
-
-                    <button className={styles.logout}>Logout</button>
-                    </form>
-                </>
+                {session?.user ? (
+                    <>
+                        {session.user?.isAdmin && (<NavLink item={{title: "Admin", path: "/admin"}}/>)}
+                        <form action={handleLogout}>
+                            <button className={styles.logout}>Logout</button>
+                        </form>
+                    </>
                 ) : (
                     <NavLink item={{title: "Login", path: "/login"}}/>
-                    )
-                }
-        </div>
-        <Image className={styles.menuButton} alt="" src="/menu.png" width={30} height={30} onClick={()=>setOpen(prev=>!prev)}/>
-        {open && (
-        <div className={styles.mobileLinks}>
-                {links.map((link) => (
-                    <NavLink item={link} key={link.title}/>
-                    ))}
+                )}
             </div>
-        )}
-    </div>
+            <Image className={styles.menuButton} alt="" src="/menu.png" width={30} height={30} onClick={()=>setOpen(prev=>!prev)}/>
+            {open && (
+                <div className={styles.mobileLinks} onClick={()=>setOpen(prev=>!prev)}>
+                    {links.map((link) => (
+                        <NavLink item={link} key={link.title}/>
+                    ))}
+                    {session?.user ? (
+                        <>
+                            {session.user?.isAdmin && (<NavLink item={{title: "Admin", path: "/admin"}}/>)}
+                            <form action={handleLogout} onClick={(e) => e.stopPropagation()}>
+                                <button className={styles.logout}>Logout</button>
+                            </form>
+                        </>
+                    ) : (
+                        <NavLink item={{title: "Login", path: "/login"}}/>
+                    )}
+                </div>
+            )}
+        </div>
     )
 }
 
