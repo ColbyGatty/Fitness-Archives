f7d2d16e7f34cc91228597597a59100a34be79fb
Colby Gatty <colbysocials@gmail.com>
2024-04-30 16:19:13 -0700

    fix blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,36 +4,41 @@ import { getPosts } from '@/lib/data';
 
 //FETCH DATA FROM API
 const getData = async () => {
-  const res = await fetch("https://fitness-archives.vercel.app/api/blog", {cache: 'no-store'})
+  const res = await fetch("https://fitness-archives.vercel.app/api/blog", {cache: 'force-no-store'})
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
 
   return res.json()
-
 }
 
-const BlogPage = async () => {
-  
-  //FETCH DATA FROM API
+export async function getStaticProps() {
   const posts = await getData()
 
   // Reverse the order of posts
   const reversedPosts = posts.reverse();
 
-  return (
-    <div>
-  
-    <div className={styles.container}>
-      {reversedPosts.map((post) => (
-        <div className={styles.post} key={post.id}>
-      <PostCard post={post}/>
-      </div>
-  ))}
-  </div>
-  </div>
-  );
+  return {
+    props: {
+      reversedPosts,
+    },
+    revalidate: 1, // Revalidate every 1 second
+  }
 }
 
-export default BlogPage
+const BlogPage = ({ reversedPosts }) => {
+  return (
+    <div>
+      <div className={styles.container}>
+        {reversedPosts.map((post) => (
+          <div className={styles.post} key={post.id}>
+            // ...
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default BlogPage;
