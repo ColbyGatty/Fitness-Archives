2b1a31b911f735938d9219686e0c041e4ade2dfa
Colby Gatty <colbysocials@gmail.com>
2024-05-01 17:24:10 -0700

    Added contact functionality

---
+++ src/app/api/send/route.js
@@ -0,0 +1,34 @@
+
+import nodemailer from 'nodemailer';
+import { NextResponse } from 'next/server';
+import url from 'url';
+
+export async function GET(req) {
+  const { query } = url.parse(req.url, true);
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL_USER, // sender address
+    to: process.env.EMAIL_USER, // list of receivers
+    subject: 'Contact Form Message', // Subject line
+    text: `Name: ${query.name}\nEmail: ${query.email}\nPhone: ${query.phone}\nMessage: ${query.message}`, // plain text body
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent');
+    // Return a 'Thank You' message with a link to go back and style the thank you message
+    return NextResponse.redirect('https://fitness-archives.vercel.app'); 
+
+  } catch (error) {
+    console.log(error);
+    return new NextResponse('Failed to send email', { status: 500 });
+  }
+};
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,30 +2,23 @@ import Image from 'next/image'
 import styles from './contact.module.css'
 import LottieMail from '@/components/lottieAnimation/LottieMail';
 
-export const metadata = {
-  title: "Contact Page",
-  description: "Contact description",
-};
-
 const ContactPage = () => {
-
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <LottieMail className={styles.img} alt="Image" fill/>
       </div >
       <div className={styles.formContainer}>
-      <h1 className={styles.title}>Connect with Me!</h1>
+        <h1 className={styles.title}>Connect with Me!</h1>
         <p className={styles.description}>I love hearing from fellow fitness enthusiasts and researchers! 
           If you have questions about any content, suggestions for topics,
            or just want to chat about the science of lifting, please do not hesitate to reach out. 
            Use the form below to send a message.<br/><br/></p>   
-           <form action="/api/send" method="POST" className={styles.form}>
-          <input type="hidden" name="_subject" value="Fitness Archives" />
-          <input type="text" name="_name" placeholder="Name" required/>
-          <input type="text" name="email" placeholder="Email Address" required/>
-          <input type="text" name="_phone" placeholder="Phone Number (Optional)"/>
-          <textarea name="message" id='' cols='30' rows='10' required placeholder='Message' className={styles.textarea}></textarea>
+        <form action="/api/send" method="GET" className={styles.form}>
+          <input type="text" id="name" name="name" placeholder="Name" />
+          <input type="text" id="email" name="email" placeholder="Email Address" />
+          <input type="text" id="phone" name="phone" placeholder="Phone Number (Optional)" />
+          <textarea id="message" name="message" cols='30' rows='10' placeholder='Message' className={styles.textarea}></textarea>
           <button type="submit" className={styles.button}>Send</button>
         </form>
       </div>
@@ -33,4 +26,4 @@ const ContactPage = () => {
   )
 }
 
-export default ContactPage
+export default ContactPage;
